c8e8df59baed9b718f9107de450eca763b5534ab
Andrew Szot <me@andrewszot.com>
2017-01-12 17:34:32 -0800

    Ready to demo

--- Phorum/AppDelegate.swift
+++ Phorum/AppDelegate.swift
@@ -22,6 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         print("Configuring")
         FIRApp.configure()
         Fabric.with([Digits.self])
+
         return true
     }
 
